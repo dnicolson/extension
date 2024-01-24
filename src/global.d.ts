af29f3f473ef387416f16899ba5e0a72e89ebdb0
VampireChicken12 <Johnbrown51@yahoo.com>
2024-01-24 18:42:36 -0500

    testing stuff WIP
    (cherry picked from commit 4f99b2f1eed7360099da0c141810b02e8ce0b742)

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,7 +3,7 @@ import type { $ZodIssue } from "zod/v4/core/errors.d.cts";
 import type { AudioEngine } from "@/src/utils/audioEngine";
 
 import type { i18nInstanceType } from "./i18n";
-import type { Nullable, YouTubeNavigateStart } from "./types";
+import type { Nullable, YouTubeNavigateStart, YoutubePlayerQualityLabel } from "./types";
 
 declare module "*.svg" {
 	import React from "react";
